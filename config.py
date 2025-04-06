b3433baa5ebc161d910653637ac7d0f7eb5c4b88
RKLIFTS <maher.hato1986@gmail.com>
2025-04-06 21:04:32 +0300

    Fix: remove invalid db_type param

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///rklifts.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:yourpassword@yamanote.proxy.rlwy.net:32743/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
