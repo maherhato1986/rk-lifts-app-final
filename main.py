6d996c5b1982e2dcc155ff6e757e009ee863326e
RKLIFTS <maher.hato1986@gmail.com>
2025-04-06 21:25:48 +0300

    Final fix for production deployment

--- main.py
+++ main.py
@@ -44,6 +44,4 @@ def admin_dashboard():
 def technician_dashboard():
     return render_template('technician_dashboard.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
